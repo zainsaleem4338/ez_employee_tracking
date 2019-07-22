d650c05fcbb5667e08a76884baa266cd03bf51ba
mujeeb-arshad <mujeeb.arshad@7vals.com>
2019-07-22 16:00:29 +0500

    Fixed Employee Creation

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,5 +1,5 @@
-# FactoryGirl.define do
-#   factory :company, :class => 'Company' do |f|
-#     f.name "Envisage"
-#   end
-# end
+FactoryGirl.define do
+  factory :company, :class => 'Company' do |f|
+    f.name "Envisage"
+  end
+end
