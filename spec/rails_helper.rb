9c156143c22dddd7de28b0d05f37297c5e0f076e
Muhammad Abdullah Khalil <muhammadabdullahkhalil@gmail.com>
2019-07-24 12:42:18 +0500

    resolved conflicts

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,7 +22,6 @@ require 'shoulda/matchers'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
