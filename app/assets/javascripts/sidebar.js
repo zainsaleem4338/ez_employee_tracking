ade9fd2c70a888b25eb4d7c288d93e8b4d19d3df
tehreemtalat7 <tehreem.talat@7vals.com>
2019-07-25 16:53:40 +0500

    Updated views, added toggleable scrollbar, fixed top bar

---
+++ app/assets/javascripts/sidebar.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  $(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+      $('#sidebar, #content, #_navbar').toggleClass('active');
+    });
+  });
+
+});
+
