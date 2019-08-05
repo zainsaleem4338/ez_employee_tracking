c049174c6ae71e5010813bba42d4bf9378def315
tehreemtalat7 <tehreem.talat@7vals.com>
2019-08-05 16:27:32 +0500

    resloved issues

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,17 +1,4 @@
 module EmployeesHelper
-  def not_team_task?(task)
-    !task.assignable_type.eql? 'Team'
-  end
-
-  def team_task?(task)
-    return false if task.eql? -1
-    task.assignable_type.eql? 'Team'
-  end
-
-  def no_current_task?(task)
-    task.eql? -1
-  end
-
   def employee_velocity_nan?(employee_velocity)
     if employee_velocity.to_f.nan? || (employee_velocity == (Float::INFINITY))
       return '-'
