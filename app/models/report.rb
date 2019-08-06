103adbd17d3d1c9ed874189eaacf11bbf1c42ff9
Muhammad Abdullah Khalil <muhammadabdullahkhalil@gmail.com>
2019-08-06 16:25:56 +0500

    Update report.rb

--- app/models/report.rb
+++ app/models/report.rb
@@ -69,20 +69,22 @@ class Report
     week_days = [1, 2, 3, 4, 5]
     attendances_array = []
     current_employee.company.employees.each do |employee|
-      @attendances = employee.attendances.where('status = ?', Attendance::STATUS[:PRESENT]).order(login_time: :asc).select { |attendance| attendance.login_time.year == Date.today.year && attendance.logout_time != nil }
-      expected_working_days = (@attendances.first.login_time.to_date..@attendances.last.login_time.to_date).to_a.select { |k| week_days.include?(k.wday) }.count
-      leaves = (end_date.month - @attendances.first.login_time.month + 1) * 2
-      actual_working_days = @attendances.count
-      half_days = @attendances.select { |attendance| ((attendance.logout_time - attendance.login_time) / 3600) <= 6 }.count
-      full_days = actual_working_days - half_days
-      absents = expected_working_days - actual_working_days
-      attendances_array << {
-        employee: employee,
-        presents: full_days,
-        half_days: half_days,
-        absents: absents,
-        leaves_remaining: leaves - absents - (half_days / 2).floor
-      }
+      @attendances = employee.attendances.where(status: Attendance::STATUS[:PRESENT]).order(login_time: :asc).select { |attendance| attendance.login_time.year == Date.today.year && attendance.logout_time != nil }
+      if !@attendances.nil?
+        expected_working_days = (@attendances.first.login_time.to_date..@attendances.last.login_time.to_date).to_a.select { |k| week_days.include?(k.wday) }.count
+        leaves = (end_date.month - @attendances.first.login_time.month + 1) * 2
+        actual_working_days = @attendances.count
+        half_days = @attendances.select { |attendance| ((attendance.logout_time - attendance.login_time) / 3600) <= 6 }.count
+        full_days = actual_working_days - half_days
+        absents = expected_working_days - actual_working_days
+        attendances_array << {
+          employee: employee,
+          presents: full_days,
+          half_days: half_days,
+          absents: absents,
+          leaves_remaining: leaves - absents - (half_days / 2).floor
+        }
+      end
     end
     attendances_array
   end
